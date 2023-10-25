e2d6c4e4ed3ee26e33d4ccd2bfb9651af081eb31
LaloLalo1999 <eduardo.lalo1999@gmail.com>
2023-10-24 18:12:24 -0600

    Added validation middleware to router

--- app/controllers/router.js
+++ app/controllers/router.js
@@ -6,9 +6,19 @@ const productRouter = require("./../routes/products");
 const adminProductRouter = require("./../routes/admin_products");
 
 router.use("/products", productRouter);
+router.use("/admin/products", validateAdmin, adminProductRouter);
 
 router.get('/', (req, res) => {
   res.send('E-Commerce app Practica 3');
 });
+
+function validateAdmin(req, res, next) {
+  let adminToken = req.get('x-auth');
+  if (adminToken == undefined || adminToken != 'admin') {
+    res.status(403).send('Unauthorized');
+  } else {
+    next();
+  }
+}
 
 module.exports = router;
